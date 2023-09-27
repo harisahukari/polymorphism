336f6cc56fc9e1351bb3632254937ac4ef4b146c
harisahukari <sahukarihari3@gmail.com>
2023-09-27 16:04:26 +0530

    six commit

---
+++ src/Finalkeyword/finaltest.java
@@ -0,0 +1,11 @@
+package Finalkeyword;
+
+public class finaltest {
+
+	public static void main(String[] args) {
+	 fine1 f = new fine1();
+	     System.out.println(f.PI*3);
+
+	}
+
+}
---
+++ src/Finalkeyword/fine1.java
@@ -0,0 +1,5 @@
+package Finalkeyword;
+
+public class fine1 {
+   final double PI = 3.14;
+}
